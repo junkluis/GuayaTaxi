e991cdcb4b82cf256dd197e7cf8ae9b8a0a28d99
Cliente CONTROL-PC <CltControl@GGIECSOFWRK154.espol.edu.ec>
2019-05-28 11:38:56 -0500

    Taller Jorge Rodriguez

--- Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
+++ Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
@@ -12,12 +12,36 @@ public class NuevoChofer {
 
     @Test
     public void choferJoven() {
+        //edad minimo 25 años
         boolean chofer_valido = MainActivity.VerificarChofer( "Luis Zuniga",
                                                                 "GYZ1234",
                                                                      "Nissan",
                                                                  25,
                                                                       "Sanborondon");
         assertTrue(chofer_valido);
+        //edad maximo 65 años
+        boolean chofer_valido2 = MainActivity.VerificarChofer( "Luis Zuniga",
+                "GYZ1234",
+                "Nissan",
+                65,
+                "Samborondon");
+        assertTrue(chofer_valido2);
+
+        //Nombre con minimo 5 letras
+        boolean chofer_valido3 = MainActivity.VerificarChofer( "Jorge",
+                "GSW4565",
+                "KIA",
+                26,
+                "Norte");
+        assertTrue(chofer_valido3);
+
+        //Nombre con maximo 20 letras
+        boolean chofer_valido4 = MainActivity.VerificarChofer( "Jorge Luis Rodriguez",
+                "GSW4565",
+                "Honda",
+                26,
+                "Norte");
+        assertTrue(chofer_valido4);
     }
 
     @Test
@@ -25,7 +49,18 @@ public class NuevoChofer {
         double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
                                                                     25,
                                                                         "Samborondon");
-        assertEquals(300, sueldo_taxista, 2);
+        assertEquals(270, sueldo_taxista, 2);
+        //
+        double sueldo_taxista2 = MainActivity.CalcularSueldoChofer("GSW4565",
+                26,
+                "Norte");
+        assertEquals(300, sueldo_taxista2, 2);
+
+        //50 dolares extras al sueldo por ser mayor de 50 años
+        double sueldo_taxista3 = MainActivity.CalcularSueldoChofer("GSW4565",
+                51,
+                "Norte");
+        assertEquals(350, sueldo_taxista3, 2);
     }
 
 
