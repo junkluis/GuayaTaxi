72aaf0f5f0d61aba0e7f388ba2a5c2bcaecf233a
Cliente CONTROL-PC <CltControl@GGIECSOFWRK146.espol.edu.ec>
2019-05-28 11:28:27 -0500

    Pruebas Unitarias

--- Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
+++ Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
@@ -12,21 +12,74 @@ public class NuevoChofer {
 
     @Test
     public void choferJoven() {
-        boolean chofer_valido = MainActivity.VerificarChofer( "Luis Zuniga",
-                                                                "GYZ1234",
-                                                                     "Nissan",
-                                                                 25,
-                                                                      "Sanborondon");
+        boolean chofer_valido = MainActivity.VerificarChofer("Luis Zuniga",
+                "GYZ1234",
+                "Nissan",
+                25,
+                "Sanborondon");
         assertTrue(chofer_valido);
     }
 
     @Test
     public void SueldoChoferJoven() {
         double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
-                                                                    25,
-                                                                        "Samborondon");
+                25,
+                "Samborondon");
         assertEquals(300, sueldo_taxista, 2);
     }
 
+    @Test
+    public void CP1() {
+        boolean chofer_valido = MainActivity.VerificarChofer("Luis Zuniga",
+                "XYZ1234",
+                "Nissan",
+                60,
+                "Norte");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void CP2() {
+        boolean chofer_valido = MainActivity.VerificarChofer("Lu Zuniga",
+                "XYZ124",
+                "Chevrolet",
+                75,
+                "Milagro");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void CP3() {
+        boolean chofer_valido = MainActivity.VerificarChofer("Luis Zuniga",
+                "XYZ1234",
+                "Chevrolet",
+                25,
+                "Norte");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void CP4() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
+                25,
+                "Samborondon");
+        assertEquals(270, sueldo_taxista, 2);
+    }
+
+    @Test
+    public void CP5() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("PYZ1234",
+                52,
+                "Norte");
+        assertEquals(400, sueldo_taxista, 2);
+    }
+
+    @Test
+    public void CP6() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
+                24,
+                "Norte");
+        assertEquals(300, sueldo_taxista, 2);
+    }
 
 }
