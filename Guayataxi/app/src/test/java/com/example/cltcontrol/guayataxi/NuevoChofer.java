9b85779800bc6712a686a9107a3fa15c98590fbf
rrobayo <rrobayo@espol.edu.ec>
2019-05-28 11:39:29 -0500

    taller

--- Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
+++ Guayataxi/app/src/test/java/com/example/cltcontrol/guayataxi/NuevoChofer.java
@@ -12,11 +12,41 @@ public class NuevoChofer {
 
     @Test
     public void choferJoven() {
-        boolean chofer_valido = MainActivity.VerificarChofer( "Luis Zuniga",
+        boolean chofer_valido = MainActivity.VerificarChofer( "Luis",
                                                                 "GYZ1234",
                                                                      "Nissan",
-                                                                 25,
-                                                                      "Sanborondon");
+                                                                 30,
+                                                                      "Norte");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void choferJoven2() {
+        boolean chofer_valido = MainActivity.VerificarChofer( "Luis Guevara",
+                "GYZ1234",
+                "Nissan",
+                50,
+                "Samborondon");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void choferJoven3() {
+        boolean chofer_valido = MainActivity.VerificarChofer( "Luis Guevara pEDRO zUMIZATA",
+                "GYZ1234",
+                "Nissan",
+                70,
+                "Oeste");
+        assertTrue(chofer_valido);
+    }
+
+    @Test
+    public void choferJoven4() {
+        boolean chofer_valido = MainActivity.VerificarChofer( "Luis ",
+                "GYZH234",
+                "Nissan",
+                55,
+                "Samborondon");
         assertTrue(chofer_valido);
     }
 
@@ -25,6 +55,30 @@ public class NuevoChofer {
         double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
                                                                     25,
                                                                         "Samborondon");
+        assertEquals(300, sueldo_taxista, 2);
+    }
+
+    @Test
+    public void SueldoChoferJoven2() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
+                30,
+                "Norte");
+        assertEquals(300, sueldo_taxista, 2);
+    }
+
+    @Test
+    public void SueldoChoferJoven3() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
+                50,
+                "Samborondon");
+        assertEquals(300, sueldo_taxista, 2);
+    }
+
+    @Test
+    public void SueldoChoferJoven4() {
+        double sueldo_taxista = MainActivity.CalcularSueldoChofer("GYZ1234",
+                25,
+                "Samborondon");
         assertEquals(300, sueldo_taxista, 2);
     }
 
